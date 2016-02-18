c84bf36f26e8dcc09be431180c6cac7a3431c9ce
Sung Kim <skim928@gatech.edu>
2016-02-18 12:32:19 -0500

    initial commit

---
+++ iOS/Skeju/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Skeju
+//
+//  Created by 김성식 on 2/18/16.
+//  Copyright © 2016 GeorgiaTech. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

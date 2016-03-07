e541da589b33af98d4438d21c3a548c0dbaa5f5b
Sung Kim <skim928@gatech.edu>
2016-03-06 23:46:17 -0500

    added tabs. skipped login for now.

---
+++ iOS/Skeju/CalendarController.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarController.swift
+//  Skeju
+//
+//  Created by 김성식 on 3/6/16.
+//  Copyright © 2016 GeorgiaTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CalendarController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

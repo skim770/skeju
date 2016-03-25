139d63b2650323bf8f27654be2213d5102991af6
Hanbeen Kim <hb24a@gatech.edu>
2016-03-25 04:11:45 -0400

    FindPoeple
    changed the view control for find friend

--- iOS/Skeju/FindPeopleController.swift
+++ iOS/Skeju/FindPeopleController.swift
@@ -10,7 +10,38 @@ import Foundation
 import UIKit
 
 class FindPeopleController: UIViewController {
+    
+    @IBOutlet var dayPlannerContainer: UIView!
+    @IBOutlet var beginTimeView: UIView!
+    @IBOutlet var endTimeView: UIView!
+    @IBOutlet var foundFriendView: UIView!
+    
+    var dayPlannerController: DayPlannerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        dayPlannerController = DayPlannerController(eventStore: EKEventStore())
+        dayPlannerController.calendar = NSCalendar.currentCalendar()
+        
+        self.addChildViewController(dayPlannerController)
+        self.dayPlannerContainer.addSubview(dayPlannerController.view)
+        dayPlannerController.view.frame = self.dayPlannerContainer.bounds
+        dayPlannerController.didMoveToParentViewController(self)
     }
 }
+
+/*
+// Get Friend List
+FBRequestConnection.startForMyFriendsWithCompletionHandler({ (connection, result, error: NSError!) -> Void in
+if error == nil {
+var friendObjects = result["data"] as [NSDictionary]
+for friendObject in friendObjects {
+println(friendObject["id"] as NSString)
+}
+println("\(friendObjects.count)")
+} else {
+println("Error requesting friends list form facebook")
+println("\(error)")
+}
+})
+*/

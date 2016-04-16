929ff7536079d9c8f0641bb752a7831b1d3b64a4
Hanbeen Kim <hb24a@gatech.edu>
2016-04-16 19:47:15 -0400

    Find by Time
    randomly displays friend to the tableview when time changed. (50%
    chance of getting displayed).
    Check if the time is future to past and automatically change it to
    display same time if selected time is future to past.

--- iOS/Skeju/FindPeopleController.swift
+++ iOS/Skeju/FindPeopleController.swift
@@ -11,7 +11,7 @@ import UIKit
 import EventKit
 import FBSDKCoreKit
 
-class FindPeopleController: UIViewController {
+class FindPeopleController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var titleView: UIView!
     @IBOutlet var dayPlannerContainer: UIView!
@@ -20,15 +20,18 @@ class FindPeopleController: UIViewController {
     @IBOutlet var endTimeView: UIView!
     @IBOutlet var foundFriendView: UIView!
     
-    @IBOutlet weak var beginDate: UILabel!
-    @IBOutlet weak var endDate: UILabel!
-    @IBOutlet weak var beginTime: UIButton!
-    @IBOutlet weak var endTime: UIButton!
-    @IBOutlet weak var beginDay: UILabel!
-    @IBOutlet weak var endDay: UILabel!
+    @IBOutlet var beginDate: UILabel!
+    @IBOutlet var endDate: UILabel!
+    @IBOutlet var beginTime: UIButton!
+    @IBOutlet var endTime: UIButton!
+    @IBOutlet var beginDay: UILabel!
+    @IBOutlet var endDay: UILabel!
+    var beginAll: String = ""
+    var endAll: String = ""
     
-    @IBOutlet weak var fbFriendList: UITableView!
+    @IBOutlet var fbFriendList: UITableView!
     var fbFriendsListData = [(name: String, id: String, profile: UIImage)]()
+    let userDefault = NSUserDefaults()
     
     
     let screenWidth = UIScreen.mainScreen().bounds.width
@@ -40,12 +43,37 @@ class FindPeopleController: UIViewController {
     let datePicker : UIDatePicker = UIDatePicker()
     
     @IBAction func beginChange(sender: AnyObject) {
-        
         timeChange(true, pos: beginTime.center.x - beginTime.frame.width/2)
     }
     
     @IBAction func endChange(sender: AnyObject) {
         timeChange(false, pos: endTime.center.x - endTime.frame.width/2)
+    }
+    
+    override func viewDidLoad() {
+        //self.scene.scaleMode = SKSceneScaleMode.ResizeFill
+        
+        super.viewDidLoad()
+        dayPlannerController = DayPlannerController(eventStore: EKEventStore())
+        dayPlannerController.calendar = NSCalendar.currentCalendar()
+        
+        self.addChildViewController(dayPlannerController)
+        self.dayPlannerContainer.addSubview(dayPlannerController.view)
+        dayPlannerController.view.frame = self.dayPlannerContainer.bounds
+        dayPlannerController.didMoveToParentViewController(self)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM:dd:yyyy:h:m"
+        beginAll = dateFormatter.stringFromDate(NSDate())
+        endAll = dateFormatter.stringFromDate(NSDate())
+        
+        changeDate(beginDay, date: beginDate, time: beginTime, datePicked: NSDate())
+        changeDate(endDay, date: endDate, time: endTime, datePicked: NSDate())
+        
+        fbFriendList.delegate = self
+        fbFriendList.dataSource = self
+        
+        getFriends()
     }
     
     func timeChange(i: Bool, pos: CGFloat) {
@@ -75,32 +103,69 @@ class FindPeopleController: UIViewController {
         
     }
     
-    func dismissPickerBegin(sender: UIButton) {
+    func changeDate(day: UILabel, date: UILabel, time: UIButton, datePicked: NSDate) {
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "cccc"
-        beginDay.text = dateFormatter.stringFromDate(datePicker.date)
+        day.text = dateFormatter.stringFromDate(datePicked)
         dateFormatter.dateFormat = "MMM dd"
-        beginDate.text = dateFormatter.stringFromDate(datePicker.date)
+        date.text = dateFormatter.stringFromDate(datePicked)
         dateFormatter.dateFormat = "h:mm a"
-        beginTime.setTitle(dateFormatter.stringFromDate(datePicker.date), forState: .Normal)
+        time.setTitle(dateFormatter.stringFromDate(datePicked), forState: .Normal)
+    }
+    
+    func dateCheck() -> Bool {
+        let endDates = endAll.componentsSeparatedByString(":")
+        let beginDates = beginAll.componentsSeparatedByString(":")
+        
+        if beginDates[2] > endDates[2] {
+            return true
+        } else if beginDates[0] > endDates[0] {
+            return true
+        } else if beginDates[1] > endDates[1] {
+            return true
+        } else if beginDates[3] > endDates[3] {
+            return true
+        } else if beginDates[4] > endDates[4] {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func dismissPickerBegin(sender: UIButton) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM:dd:yyyy:h:m"
+        beginAll = dateFormatter.stringFromDate(datePicker.date)
+        changeDate(beginDay, date: beginDate, time: beginTime, datePicked: datePicker.date)
+        
         for view in self.datePickerContainer.subviews {
             view.removeFromSuperview()
         }
         datePickerContainer.removeFromSuperview()
+        
+        if dateCheck() {
+            endAll = dateFormatter.stringFromDate(datePicker.date)
+            changeDate(endDay, date: endDate, time: endTime, datePicked: datePicker.date)
+        }
+        getFriends()
     }
     
     func dismissPickerEnd(sender: UIButton) {
         let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "cccc"
-        endDay.text = dateFormatter.stringFromDate(datePicker.date)
-        dateFormatter.dateFormat = "MMM dd"
-        endDate.text = dateFormatter.stringFromDate(datePicker.date)
-        dateFormatter.dateFormat = "h:mm a"
-        endTime.setTitle(dateFormatter.stringFromDate(datePicker.date), forState: .Normal)
+        dateFormatter.dateFormat = "dd:MM:yyyy:HH:mm"
+        endAll = dateFormatter.stringFromDate(datePicker.date)
+        changeDate(endDay, date: endDate, time: endTime, datePicked: datePicker.date)
+        
         for view in self.datePickerContainer.subviews {
             view.removeFromSuperview()
         }
         datePickerContainer.removeFromSuperview()
+
+        if dateCheck() {
+            beginAll = dateFormatter.stringFromDate(datePicker.date)
+            changeDate(beginDay, date: beginDate, time: beginTime, datePicked: datePicker.date)
+        }
+        getFriends()
     }
     
     func getFriends() {
@@ -112,50 +177,27 @@ class FindPeopleController: UIViewController {
                 let friendsDict = result as! NSDictionary
                 let data : NSArray = friendsDict.objectForKey("data") as! NSArray
                 
+                self.fbFriendsListData.removeAll()
+                
                 for i in 0..<data.count {
-                    let valueDict : NSDictionary = data[i] as! NSDictionary
-                    let fname = valueDict.objectForKey("name") as! String
-                    let fid = valueDict.objectForKey("id") as! String
-                    
-                    let fbPicURL = "https://graph.facebook.com/\(fid)/picture?type=normal&return_ssl_resources=1"
-                    let fbRequest = NSURL(string: fbPicURL)
-                    let fbProfilePicData = NSData(contentsOfURL: fbRequest!)
-                    let fbProfilePic = UIImage(data: fbProfilePicData!)
-                    
-                    self.fbFriendsListData.append((name: fname, id: fid, profile: fbProfilePic!))
+                    let rand = arc4random_uniform(2)
+                    if rand == 1 {
+                        let valueDict : NSDictionary = data[i] as! NSDictionary
+                        let fname = valueDict.objectForKey("name") as! String
+                        let fid = valueDict.objectForKey("id") as! String
+                        
+                        let fbPicURL = "https://graph.facebook.com/\(fid)/picture?type=normal&return_ssl_resources=1"
+                        let fbRequest = NSURL(string: fbPicURL)
+                        let fbProfilePicData = NSData(contentsOfURL: fbRequest!)
+                        let fbProfilePic = UIImage(data: fbProfilePicData!)
+                        
+                        self.fbFriendsListData.append((name: fname, id: fid, profile: fbProfilePic!))
+                    }
                 }
                 
                 self.fbFriendList.reloadData()
             }
         })
-    }
-   
-    override func viewDidLoad() {
-        //self.scene.scaleMode = SKSceneScaleMode.ResizeFill
-        
-        super.viewDidLoad()
-        dayPlannerController = DayPlannerController(eventStore: EKEventStore())
-        dayPlannerController.calendar = NSCalendar.currentCalendar()
-        
-        self.addChildViewController(dayPlannerController)
-        self.dayPlannerContainer.addSubview(dayPlannerController.view)
-        dayPlannerController.view.frame = self.dayPlannerContainer.bounds
-        dayPlannerController.didMoveToParentViewController(self)
-
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "cccc"
-        beginDay.text = dateFormatter.stringFromDate(NSDate())
-        endDay.text = dateFormatter.stringFromDate(NSDate())
-
-        dateFormatter.dateFormat = "MMM dd"
-        beginDate.text = dateFormatter.stringFromDate(NSDate())
-        endDate.text = dateFormatter.stringFromDate(NSDate())
-        
-        dateFormatter.dateFormat = "h:mm a"
-        beginTime.setTitle(dateFormatter.stringFromDate(NSDate()), forState: .Normal)
-        endTime.setTitle(dateFormatter.stringFromDate(NSDate()), forState: .Normal)
-        
-        getFriends()
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
